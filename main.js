13659f015617f1c56c7ce6e772991f4a2580befd
José Tizon <josalberto314@gmail.com>
2024-01-27 05:36:01 -0300

    Basics included
    Raw html included, with some styles added and some minor javascript to enhance styles

---
+++ main.js
@@ -0,0 +1,20 @@
+const expressionInput = document.querySelector(".expression-input");
+let expression;
+
+expressionInput.addEventListener("focus", (e) => {
+  if(e.target.value == "Enter expression")
+    e.target.value = "";
+});
+
+expressionInput.addEventListener("blur", (e) => {
+  if(e.target.value == "")
+    e.target.value = "Enter expression";
+  expression = e.target.value;
+})
+
+const calculateButton = document.querySelector(".calculate-button");
+
+
+
+
+
